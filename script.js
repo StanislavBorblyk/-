f3dac207fddebe48a04a26f1bc5431540cedcf33
Stanislav <borblykstas@gmail.com>
2020-09-01 14:52:40 +0300

    друге завдання

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const personalMovieDB = {
 
 
 
-const question1 = prompt("Останній преглянутий фільм?", ""),
+/* const question1 = prompt("Останній преглянутий фільм?", ""),
       question2 = prompt("На скільки його оціните?", ""),
       question3 = prompt("Останній преглянутий фільм?", ""),
       question4 = prompt("На скільки його оціните?", "");
@@ -20,4 +20,32 @@ const question1 = prompt("Останній преглянутий фільм?", 
       personalMovieDB.movies[question1] = question2;
       personalMovieDB.movies[question3] = question4;
 
-      console.log(personalMovieDB);
+      console.log(personalMovieDB); */
+
+      for (let i = 0; i < 2; i++) {
+        const question1 = prompt("Останній преглянутий фільм?", ""),
+              question2 = prompt("На скільки його оціните?", "");
+
+        if (question1 !== null && question2 !== null && question1 !== '' && question2 !== '' && question1.length < 50) {
+            personalMovieDB.movies[question1] = question2;
+            console.log('done');
+        } else {
+            console.log('error');
+            i --;
+       }     
+       };
+            console.log(personalMovieDB);
+
+        if (personalMovieDB.count < 10) {
+            console.log('просмотрено мало фильмов');
+        }
+        else if (10 < personalMovieDB.count < 30 ) {
+            console.log('ви чудовий глядач');
+        }
+        else if (personalMovieDB.count > 30 ) {
+            console.log('ви кіноман');
+    } else {
+            console.log('сталася помилка');
+    };
+    
+ 
